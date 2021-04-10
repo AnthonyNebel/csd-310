f49a073e8c9e06f4d2ee669dc1b84556c363c967
AnthonyNebel <anthony.nebel@gmail.com>
2021-04-10 13:35:53 -0500

    module 5.2 py program commit

---
+++ module_5/mongodb_test.py
@@ -0,0 +1,23 @@
+""" 
+    mongodb_test.py test code
+    Anthony Nebel
+    10 April 2021
+    Test program for connecting to a MongoDB Atlas cluster
+"""
+
+from pymongo import MongoClient
+
+
+url = "mongodb+srv://admin:admin@cluster0.djd2z.mongodb.net/pytech?retryWrites=true&w=majority"
+
+
+client = MongoClient(url)
+
+db = client.pytech
+
+
+print("\n -- Pytech COllection List --")
+print(db.list_collection_names())
+
+
+input("\n\n  End of program, press any key to exit... ")
